fb57975ad9fa65b746bfe9db065a3777244660b4
Mike Doswell <mike.doswell@gmail.com>
2025-01-12 15:47:03 -0800

    added button scrambling

--- COMP4537/labs/0/js/script.js
+++ COMP4537/labs/0/js/script.js
@@ -1,6 +1,6 @@
 // import messages from '../lang/messages/en/user.js';
 
-const messages = {prompt: "How many buttons to create?", controlText: "Go!", invalidInput: "Invalid input", success: "Excellent memory!", failure: "Wrong order!"};
+const messages = {invalidInput: "Invalid input", success: "Excellent memory!", failure: "Wrong order!"};
 
 class Control {
     constructor(input, button, buttonDiv) {
@@ -27,35 +27,83 @@ class Control {
 
 class Game {
     constructor(n, buttonDiv) {
-        this.n = n;
+        this.nScrambles = n;
         this.buttons = [];
+        const buttonDivRect = buttonDiv.getBoundingClientRect();
         for (let i = 0; i < n; i++) {
-            let button = new Button(i, '0px', `${i * 200}px`)
+            let button = new Button(i + 1, `${buttonDivRect.top}px`, `${i * 11 + 1}em`)
             this.buttons.push(button);
             buttonDiv.appendChild(button.button);
         }
+        this.scrambleTimeout = setTimeout(this.startScramble, 1000 * n, this);
+    }
+
+    startScramble(game) {
+        game.scrambleInterval = setInterval(game.updateButtons, 2000, game);
+    }
+
+    updateButtons(game) {
+        game.buttons.forEach(button => {
+            button.randomizePosition();
+        })
+        if (--game.nScrambles <= 0) {
+            clearInterval(game.scrambleInterval);
+            game.startGuessing();
+            return;
+        }
+    }
+
+    startGuessing() {
+        this.buttons.forEach(button => {
+            button.hideNumber();
+            button.makeClickable();
+        })
+        this.nextButton = 1;
     }
 }
 
 class Button {
     constructor(number, top, left) {
         this.button = document.createElement('button');
+        this.button.disabled = true;
+        this.button.onclick = this.guess.bind(this);
         this.button.classList.add('memory-button');
         this.button.style.top = top;
         this.button.style.left = left;
-        this.button.innerHTML = number + 1;
+        this.button.innerHTML = number;
         this.button.style.backgroundColor = this.getRandomColor();
     }
 
+    guess() {
+        this.button.style.opacity = 1;
+    }
+
     getRandomColor() {
-        return `rgb(${255}, ${99}, ${71})`
+        return `rgb(${this.getRandomVal(255)}, ${this.getRandomVal(255)}, ${this.getRandomVal(255)})`
+    }
+
+    getRandomVal(max) {
+        return Math.floor(Math.random() * (max + 1));
+    }
+
+    randomizePosition() {
+        this.button.style.position = 'absolute';
+        this.button.style.top = this.getRandomVal(window.innerHeight - this.button.clientHeight);
+        this.button.style.left = this.getRandomVal(window.innerWidth - this.button.clientWidth);
+    }
+
+    makeClickable() {
+        this.button.disabled = false;
+    }
+
+    hideNumber() {
+        this.button.innerHTML = '';
     }
 }
 
-let control;
+// let control;
 
 initializeGame = function() {
-    control = new Control('control-input', 'control-button', 'button-div');
-    // document.getElementById('feedback-message').innerHTML = messages.success;
+    const control = new Control('control-input', 'control-button', 'button-div');
 }
 
