2f6d3cbb322358cbcfc9b1c37c17f60c787dc8ec
Mike Doswell <mike.doswell@gmail.com>
2025-01-12 16:48:46 -0800

    switching messages to external file

--- COMP4537/labs/0/lang/messages/en/user.js
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -1,3 +1,3 @@
 const messages = {prompt: "How many buttons to create?", invalidInput: "Invalid input", success: "Excellent memory!", failure: "Wrong order!"};
 
-export {messages};
+export default messages;
