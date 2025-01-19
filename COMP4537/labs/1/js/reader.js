62a112562bb5b7ee73ba51250a63ecdac28e1441
Mike Doswell <mike.doswell@gmail.com>
2025-01-19 13:27:53 -0800

    implementing Writer and Note classes

--- COMP4537/labs/1/js/reader.js
+++ COMP4537/labs/1/js/reader.js
@@ -1,3 +1,6 @@
+let messages = {noStorage: 'localStorage not supported on this browser.', storedAt: 'Stored at: ', buttonLabelRemove: 'Remove'}
+
+
 document.addEventListener("DOMContentLoaded", initializeReader);
 
 function initializeReader() {
@@ -5,9 +8,63 @@ function initializeReader() {
     //     const stored = localStorage.getItem("writerKey");
     // }
     window.addEventListener("storage", event => {
-        alert("stored");
+        console.log("stored");
     })
     window.onstorage = (event) => {
-        alert("store");
+        console.log("store");
+    }
+}
+
+class Reader {
+    constructor(noteContainer, storeTimeText) {
+        this.noteContainer = noteContainer;
+        this.storeTimeText = storeTimeText;
+        this.notes = [];
+        this.getNotes();
+    }
+
+    getNotes() {
+        if (typeof Storage !== "undefined") {
+            const notes = JSON.parse(localStorage.getItem("writerKey"));
+            notes.forEach(note => {
+                this.addNote(note.text);
+            });
+        } else {
+            alert(messages.noStorage);
+        }
+    }
+}
+
+class Note {
+    constructor(noteContainer, text, storageCallback, removeCallback) {
+        this.noteContainer = noteContainer;
+        this.text = text;
+        this.storageCallback = storageCallback;
+        this.removeCallback = removeCallback;
+        this.container = this.createElements();
+        noteContainer.appendChild(this.container);
+    }
+
+    createElements() {
+        const container = document.createElement('div');
+        const field = document.createElement('textarea');
+        const button = document.createElement('button');
+        field.innerHTML = this.text;
+        field.oninput = () => this.update(field);
+        button.innerHTML = messages.buttonLabelRemove;
+        button.onclick = () => this.remove();
+        container.appendChild(field);
+        container.appendChild(button);
+        return container;
+    }
+
+    remove() {
+        this.removeCallback(this);
+        this.container.remove();
+    }
+
+    update(field) {
+        this.text = field.value;
+        this.storageCallback();
     }
 }
--- COMP4537/labs/1/js/writer.js
+++ COMP4537/labs/1/js/writer.js
@@ -1,23 +1,83 @@
+let messages = {noStorage: 'localStorage not supported on this browser.', storedAt: 'Stored at: ', buttonLabelRemove: 'Remove'}
+
 document.addEventListener("DOMContentLoaded", initializeWriter);
 
 function initializeWriter() {
-    if (typeof Storage !== "undefined") {
-        localStorage.setItem("writerKey", "44");
-    }
-    window.postMessage("testmsg");
+    // window.postMessage("testmsg");
+    const writer = new Writer(document.getElementById('notes-container'), document.getElementById('storage-time'));
 }
 
 class Writer {
-    constructor() {
-        // add button
-        // get notes
+    constructor(noteContainer, storeTimeText) {
+        this.noteContainer = noteContainer;
+        this.storeTimeText = storeTimeText;
+        this.notes = [];
+        document.getElementById("add-note-button").onclick = () => this.addNote('');
+        this.getNotes();
     }
 
-    createAddButton() {
-        
+    addNote(text) {
+        this.notes.push(new Note(this.noteContainer, text, this.storeNotes.bind(this), this.removeNote.bind(this)));
+        this.storeNotes();
     }
 
     getNotes() {
+        if (typeof Storage !== "undefined") {
+            const notes = JSON.parse(localStorage.getItem("writerKey"));
+            notes.forEach(note => {
+                this.addNote(note.text);
+            });
+        } else {
+            alert(messages.noStorage);
+        }
+    }
 
+    storeNotes() {
+        if (typeof Storage !== "undefined") {
+            localStorage.setItem("writerKey", JSON.stringify(this.notes));
+            const time = new Date();
+            this.storeTimeText.innerHTML = messages.storedAt + time.toLocaleTimeString('en-US');
+        } else {
+            alert(messages.noStorage);
+        }
+    }
+
+    removeNote(note) {
+        this.notes.splice(this.notes.indexOf(note), 1);
+        this.storeNotes();
+    }
+}
+
+class Note {
+    constructor(noteContainer, text, storageCallback, removeCallback) {
+        this.noteContainer = noteContainer;
+        this.text = text;
+        this.storageCallback = storageCallback;
+        this.removeCallback = removeCallback;
+        this.container = this.createElements();
+        noteContainer.appendChild(this.container);
+    }
+
+    createElements() {
+        const container = document.createElement('div');
+        const field = document.createElement('textarea');
+        const button = document.createElement('button');
+        field.innerHTML = this.text;
+        field.oninput = () => this.update(field);
+        button.innerHTML = messages.buttonLabelRemove;
+        button.onclick = () => this.remove();
+        container.appendChild(field);
+        container.appendChild(button);
+        return container;
+    }
+
+    remove() {
+        this.removeCallback(this);
+        this.container.remove();
+    }
+
+    update(field) {
+        this.text = field.value;
+        this.storageCallback();
     }
 }
