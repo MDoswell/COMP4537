c623bf15dec8c765369690f9837eed790d416370
Mike Doswell <mike.doswell@gmail.com>
2025-01-19 16:20:22 -0800

    fixed lab1 to work on hosted site

--- COMP4537/labs/1/js/reader.js
+++ COMP4537/labs/1/js/reader.js
@@ -1,3 +1,5 @@
+import Note from './note.js';
+
 async function getMessages() {
     const response = await fetch('./lang/messages/en/user.json')
     if (!response.ok) 
@@ -49,44 +51,3 @@ class Reader {
         this.notes = [];
     }
 }
-
-// class Note {
-//     constructor(noteContainer, text, isWritable, storageCallback, removeCallback) {
-//         this.noteContainer = noteContainer;
-//         this.text = text;
-//         this.isWritable = isWritable;
-//         this.storageCallback = storageCallback;
-//         this.removeCallback = removeCallback;
-//         this.container = this.createElements();
-//         noteContainer.appendChild(this.container);
-//     }
-
-//     createElements() {
-//         const container = document.createElement('div');
-
-//         const field = document.createElement('textarea');
-//         field.innerHTML = this.text;
-//         field.oninput = () => this.update(field);
-//         container.appendChild(field);
-
-//         if (this.isWritable) {
-//             const button = document.createElement('button');
-//             button.innerHTML = messages.buttonLabelRemove;
-//             button.onclick = () => this.remove();
-//             container.appendChild(button);
-//         }
-        
-//         return container;
-//     }
-
-//     remove() {
-//         if (this.isWritable)
-//             this.removeCallback(this);
-//         this.container.remove();
-//     }
-
-//     update(field) {
-//         this.text = field.value;
-//         this.storageCallback();
-//     }
-// }
