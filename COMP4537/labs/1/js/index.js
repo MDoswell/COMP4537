1819ef890bdaf32ab571645df61c39ad43d9443b
Mike Doswell <mike.doswell@gmail.com>
2025-01-19 16:30:03 -0800

    moved strings from index.html to messages json

---
+++ COMP4537/labs/1/js/index.js
@@ -0,0 +1,18 @@
+async function getMessages() {
+    const response = await fetch('./lang/messages/en/user.json')
+    if (!response.ok) 
+        return;
+    const data = await response.json();
+    return data;
+}
+let messages;
+
+document.addEventListener("DOMContentLoaded", initializeWriter);
+
+async function initializePage() {
+    messages = await getMessages();
+    document.getElementById("title-text").innerHTML = messages.title;
+    document.getElementById("top-header").innerHTML = messages.title;
+    document.getElementById("reader-link").innerHTML = messages.readerLink;
+    document.getElementById("writer-link").innerHTML = messages.writerLink;
+}
