da22722ff6105a9bd640ae8b6929a7d94653512a
Mike Doswell <mike.doswell@gmail.com>
2025-02-01 12:41:44 -0800

    removed writeTest

--- COMP4537/nodeLabs/3/server.js
+++ COMP4537/nodeLabs/3/server.js
@@ -4,15 +4,13 @@ const {getDate} = require('./modules/getDate')
 
 const routes = [
     {path: "/getdate", action: getDate},
-    {path: "/writefile", action: writeTest}
+    // {path: "/writefile", action: writeTest}
 ]
 
 http.createServer((req, res) => {
     const urlParts = url.parse(req.url)
     for (let i = 0; i < routes.length; i++) {
-        console.log("req url: " + req.url + ", route: " + routes[i].path)
         if (urlParts.pathname.toLowerCase() === routes[i].path || urlParts.pathname.toLowerCase() === routes[i].path + '/') {
-            console.log("match")
             return routes[i].action(req, res)
         }
     }
